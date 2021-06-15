0d34a568d04a5af9d92a6d24eb492feed9dbe79c
negbook <negbook@gmail.com>
2021-06-16 01:09:23 +0800

    Add files via upload

---
+++ nbk_minimap_untransparent/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+games {'gta5'}
+--credit:negbook QQ:747285250 
+--What makes coffee smell so good? you!
+
+client_scripts {
+'@threads/threads.lua',
+'@scaleforms/scaleforms.lua',
+'main.lua'
+}
+
+
+dependencies {
+    'threads',
+    'scaleforms',
+    'nbk_circle',
+    'nbk_minimap_untransparent_stream'
+}
---
+++ nbk_minimap_untransparent/main.lua
@@ -0,0 +1,68 @@
+local myui_width = 191 - 10
+local myui_height = 136 - 10
+
+local Grun,Gsend,Gstop = nil,nil,nil 
+local hud = nil 
+CreateThread(function()
+    Scaleforms.CallScaleformMovie('nbk_minimap_untransparent',function(run,send,stop,handle)
+        Grun,Gsend,Gstop = run,send,stop
+        TriggerEvent('nbk_circle:RequestHudDimensionsFromMyUI',myui_width,myui_height,function(obj)
+            hud = obj
+            local isBig  = IsBigmapActive()
+            local isRender  = IsMinimapRendering()
+            updateMinimap(isBig,isRender)
+        end,0.26,-0.3,false)
+    end)
+end)
+
+RegisterNetEvent("nbk_circle:OnMinimapRefresh")
+AddEventHandler('nbk_circle:OnMinimapRefresh', function(isBig,isRender)
+    TriggerEvent('nbk_circle:RequestHudDimensionsFromMyUI',myui_width,myui_height,function(obj)
+            hud = obj
+            updateMinimap(isBig,isRender)
+    end,0.26,-0.3,false)
+end)
+
+
+
+function updateMinimap(isBig,isRender)
+    if Grun and Gsend and Gstop then 
+        if isBig or not isRender then 
+            Grun("SET_RADAR_DISPLAY")
+            Gsend(false)
+            Gstop()
+            Scaleforms.EndScaleformMovie('nbk_minimap_untransparent')
+        else 
+            Grun("SET_RADAR_DISPLAY")
+            Gsend(true)
+            Gstop()
+            
+            Scaleforms.DrawScaleformMovie('nbk_minimap_untransparent',hud.x,hud.y,hud.width,hud.height, 255, 255, 255, 255,0)
+            
+        end 
+    end 
+end 
+
+
+--[=[
+CreateThread(function()
+    SetBigmapActive(
+	1 --[[ boolean ]], 
+	1 --[[ boolean ]]
+)
+Wait(3000)
+SetBigmapActive(
+	false --[[ boolean ]], 
+	1 --[[ boolean ]]
+)
+Wait(3000)
+SetBigmapActive(
+	false --[[ boolean ]], 
+	false --[[ boolean ]]
+)
+Wait(3000)
+DisplayRadar(false)
+Wait(3000)
+DisplayRadar(true)
+end)
+--]=]
